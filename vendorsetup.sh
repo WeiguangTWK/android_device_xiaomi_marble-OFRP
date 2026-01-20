77bd6af5332a6f15ae0739ec7863ad6b08f9097f
WeiguangTWK <weiguangtwk@outlook.com>
2026-01-20 21:27:59 +0800

    vendorsetup: Add marblein device

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -23,6 +23,7 @@ export OF_MAINTAINER="WeiguangTWK"
 
 export FOX_ENABLE_KERNELSU_SUPPORT=1
 export FOX_USE_ZSTD_BINARY=1
+export FOX_TARGET_DEVICES="marble,marblein"
 
 fox_get_target_device() {
 local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
